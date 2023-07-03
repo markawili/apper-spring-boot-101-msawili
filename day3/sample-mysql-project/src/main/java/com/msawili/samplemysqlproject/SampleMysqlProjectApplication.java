d52236d00f05f66cfc72f91ff524be880b313e68
Mark Awili <markandrew.awili@gcash.com>
2023-07-03 22:55:25 +0800

    initialize project

---
+++ day3/sample-mysql-project/src/main/java/com/msawili/samplemysqlproject/SampleMysqlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.msawili.samplemysqlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMysqlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMysqlProjectApplication.class, args);
+	}
+
+}
---
+++ day3/sample-mysql-project/src/test/java/com/msawili/samplemysqlproject/SampleMysqlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msawili.samplemysqlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMysqlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
