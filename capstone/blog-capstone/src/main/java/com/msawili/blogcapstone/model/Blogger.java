9a0534524e14375f4cc8d7de703a5c3e3fd9b0b9
Mark Awili <markandrew.awili@gcash.com>
2023-07-11 18:28:17 +0800

    create blogger entity

--- capstone/blog-capstone/src/main/java/com/msawili/blogcapstone/model/Blogger.java
+++ capstone/blog-capstone/src/main/java/com/msawili/blogcapstone/model/Blogger.java
@@ -1,4 +1,52 @@
 package com.msawili.blogcapstone.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "BLOGGER")
+@Data
 public class Blogger {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "COMPLETE_NAME")
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @Column(name = "LAST_UPDATE")
+    private LocalDateTime lastUpdate;
+
+    @PrePersist
+    public void setInitialTimestamp() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        lastUpdate = now;
+    }
+
+    @PreUpdate
+    public void setLastUpdate() {
+        lastUpdate = LocalDateTime.now();
+    }
+
 }
