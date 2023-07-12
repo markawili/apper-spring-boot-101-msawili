70143ceb41d8075a4e0499d17eaab169fc6c29b7
Mark Awili <markandrew.awili@gcash.com>
2023-07-12 16:23:45 +0800

    Add checking of existing email in repository

--- capstone/blog-capstone/src/main/java/com/msawili/blogcapstone/repository/BloggerRepository.java
+++ capstone/blog-capstone/src/main/java/com/msawili/blogcapstone/repository/BloggerRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BloggerRepository extends CrudRepository<Blogger, String> {
+    boolean existsByEmail(String email);
 }
